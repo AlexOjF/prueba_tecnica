b8d22955aff605b4329648d235ea24c28366e9f5
Alex Orjuela <alex.orjuela@globalseis.com>
2024-03-30 20:44:46 -0500

    Added controller and DTO

---
+++ prueba_tecnica/Controllers/MatriculaController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using prueba_tecnica.DTO;
+using prueba_tecnica.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace prueba_tecnica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatriculaController : ControllerBase
+    {
+        private readonly DBContext DBContext;
+        public MatriculaController(DBContext DBContext)
+        {
+            this.DBContext = DBContext;
+        }
+        //Se crean los métodos del CRUD
+        [HttpGet("GetMatriculas")]
+        public async Task<ActionResult<List<MatriculaDTO>>> Get()
+        {
+            var List = await DBContext.Matriculas.Select(
+                s => new MatriculaDTO
+                {
+                    Id = s.Id,
+                    FechaIncripcin = s.FechaIncripcin,
+                    LimitePago = s.LimitePago,
+                    CostoMatricula = s.CostoMatricula,
+                    Pago = s.Pago,
+                    IdAspirante = s.IdAspirante,
+                    IdPrograma = s.IdPrograma,
+                    IdAspiranteNavigation = s.IdAspiranteNavigation,
+                    IdProgramaNavigation = s.IdProgramaNavigation
+                }
+            ).ToListAsync();
+
+            if (List.Count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return List;
+            }
+        }
+
+        [HttpGet("GetMatriculaById")]
+        public async Task<ActionResult<MatriculaDTO>> GetMatriculaById(int Id)
+        {
+            MatriculaDTO Matricula = await DBContext.Matriculas.Select(
+                    s => new MatriculaDTO
+                    {
+                        Id = s.Id,
+                        FechaIncripcin = s.FechaIncripcin,
+                        LimitePago = s.LimitePago,
+                        CostoMatricula = s.CostoMatricula,
+                        Pago = s.Pago,
+                        IdAspirante = s.IdAspirante,
+                        IdPrograma = s.IdPrograma,
+                        IdAspiranteNavigation = s.IdAspiranteNavigation,
+                        IdProgramaNavigation = s.IdProgramaNavigation
+                    })
+                .FirstOrDefaultAsync(s => s.Id == Id);
+
+            if (Matricula == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Matricula;
+            }
+        }
+
+        [HttpPost("InsertMatricula")]
+        public async Task<HttpStatusCode> InsertUser(MatriculaDTO Matricula)
+        {
+            var entity = new Matricula()
+            {
+                Id = Matricula.Id,
+                FechaIncripcin = Matricula.FechaIncripcin,
+                LimitePago = Matricula.LimitePago,
+                CostoMatricula = Matricula.CostoMatricula,
+                Pago = Matricula.Pago,
+                IdAspirante = Matricula.IdAspirante,
+                IdPrograma = Matricula.IdPrograma,
+                IdAspiranteNavigation = Matricula.IdAspiranteNavigation,
+                IdProgramaNavigation = Matricula.IdProgramaNavigation,
+
+            };
+
+            DBContext.Matriculas.Add(entity);
+            await DBContext.SaveChangesAsync();
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPut("UpdateMatricula")]
+        public async Task<HttpStatusCode> UpdateMatricula(MatriculaDTO Matricula)
+        {
+            var entity = await DBContext.Matriculas.FirstOrDefaultAsync(s => s.Id == Matricula.Id);
+
+            entity.Id = Matricula.Id;
+            entity.FechaIncripcin = Matricula.FechaIncripcin;
+            entity.LimitePago = Matricula.LimitePago;
+            entity.CostoMatricula = Matricula.CostoMatricula;
+            entity.Pago = Matricula.Pago;
+            entity.IdAspirante = Matricula.IdAspirante;
+            entity.IdPrograma = Matricula.IdPrograma;
+            entity.IdAspiranteNavigation = Matricula.IdAspiranteNavigation;
+            entity.IdProgramaNavigation = Matricula.IdProgramaNavigation;
+
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+
+        [HttpDelete("DeleteMatricula/{Id}")]
+        public async Task<HttpStatusCode> DeleteMatricula(int Id)
+        {
+            var entity = new Matricula()
+            {
+                Id = Id
+            };
+            DBContext.Matriculas.Attach(entity);
+            DBContext.Matriculas.Remove(entity);
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ prueba_tecnica/Controllers/ProgramaController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using prueba_tecnica.DTO;
+using prueba_tecnica.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace prueba_tecnica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgramaController : ControllerBase
+    {
+        private readonly DBContext DBContext;
+
+        public ProgramaController(DBContext DBContext)
+        {
+            this.DBContext = DBContext;
+        }
+        //Se crean los métodos del CRUD
+        [HttpGet("GetProgramas")]
+        public async Task<ActionResult<List<ProgramaDTO>>> Get()
+        {
+            var List = await DBContext.Programas.Select(
+                s => new ProgramaDTO
+                {
+                    Id = s.Id,
+                    Descripcin = s.Descripcin,
+                    Nivel = s.Nivel,
+                }
+            ).ToListAsync();
+
+            if (List.Count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return List;
+            }
+        }
+
+        [HttpGet("GetProgramaById")]
+        public async Task<ActionResult<ProgramaDTO>> GetProgramaById(String Id)
+        {
+            ProgramaDTO Programa = await DBContext.Programas.Select(
+                    s => new ProgramaDTO
+                    {
+                        Id = s.Id,
+                        Descripcin = s.Descripcin,
+                        Nivel = s.Nivel
+                    })
+                .FirstOrDefaultAsync(s => s.Id == Id);
+
+            if (Programa == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Programa;
+            }
+        }
+
+        [HttpPost("InsertPrograma")]
+        public async Task<HttpStatusCode> InsertUser(ProgramaDTO Programa)
+        {
+            var entity = new Programa()
+            {
+                Id = Programa.Id,
+                Descripcin = Programa.Descripcin,
+                Nivel = Programa.Nivel
+            };
+
+            DBContext.Programas.Add(entity);
+            await DBContext.SaveChangesAsync();
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPut("UpdatePrograma")]
+        public async Task<HttpStatusCode> UpdatePrograma(ProgramaDTO Programa)
+        {
+            var entity = await DBContext.Programas.FirstOrDefaultAsync(s => s.Id == Programa.Id);
+
+            entity.Id = Programa.Id;
+            entity.Descripcin = Programa.Descripcin;
+            entity.Nivel = Programa.Nivel;
+
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+
+        [HttpDelete("DeletePrograma/{Id}")]
+        public async Task<HttpStatusCode> DeletePrograma(String Id)
+        {
+            var entity = new Programa()
+            {
+                Id = Id
+            };
+            DBContext.Programas.Attach(entity);
+            DBContext.Programas.Remove(entity);
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+    }
+}
